1f6eb9ab23c93a48507f435cdd806bf69232246f
Swaraj961 <swarajroutray961@gmail.com>
2020-05-26 13:06:58 +0530

    eco php basics

---
+++ Getting_started -PHP/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php test</title>
+</head>
+<body>
+    <?php
+    $var1 =18;
+    $var2 =20;
+echo "value of 1st no is :" . $var1 . " and value of 2nd no is :" . $var2
+        
+    ?>
+</body>
+</html>
