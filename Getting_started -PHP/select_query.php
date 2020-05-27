25ded0f26015888cb0a0653aa80ab36076c5f87f
Swaraj961 <swarajroutray961@gmail.com>
2020-05-28 00:18:06 +0530

    fetching data using array and select query

--- Getting_started -PHP/select_query.php
+++ Getting_started -PHP/select_query.php
@@ -7,4 +7,36 @@ $select_query_result = mysqli_query($con, $select_query) or die(mysqli_errno($co
 //to fetch no of rows 
 $fetch_rows = mysqli_num_rows($select_query_result);
 echo 'no of rows in users are = ' . $fetch_rows ;
-?>
+
+//To display the data from data base with array
+// $row = mysqli_fetch_array($select_query_result);
+// echo $row[0] . "<br>";
+// echo $row[1]  . "<br>";
+// echo $row[2]  . "<br>";
+// echo $row[3]  . "<br>";
+
+// user1
+$row = mysqli_fetch_array($select_query_result);
+echo $row['id'] . "<br>";
+echo $row['name']  . "<br>";
+echo $row['email_id']  . "<br>";
+
+// user2
+$row = mysqli_fetch_array($select_query_result);
+echo $row['id'] . "<br>";
+echo $row['name']  . "<br>";
+echo $row['email_id']  . "<br>";
+// user3
+
+$row = mysqli_fetch_array($select_query_result);
+echo $row['id'] . "<br>";
+echo $row['name']  . "<br>";
+echo $row['email_id']  . "<br>";
+// user4
+
+$row = mysqli_fetch_array($select_query_result);
+echo $row['id'] . "<br>";
+echo $row['name']  . "<br>";
+echo $row['email_id']  . "<br>";
+
+?>      
