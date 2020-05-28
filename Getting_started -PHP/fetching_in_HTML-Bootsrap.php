4c35eb4a2a4219c0e11a4a451be168c4639c51f4
Swaraj961 <swarajroutray961@gmail.com>
2020-05-28 08:08:59 +0530

    data calculation using function

--- Getting_started -PHP/fetching_in_HTML-Bootsrap.php
+++ Getting_started -PHP/fetching_in_HTML-Bootsrap.php
@@ -13,7 +13,7 @@ $select_query_result = mysqli_query($con, $select_query) or die(mysqli_errno($co
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php html bootstrap test </title>
+    <title>php html bootstrap  </title>
     <!-- <link rel="stylesheet" href="style.css"> -->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <script type="text/javascript" src="bootstrap/js/jquery-3.5.1.min.js "></script>
@@ -25,7 +25,7 @@ $select_query_result = mysqli_query($con, $select_query) or die(mysqli_errno($co
     }
     </style>
 </head>
-<body>
+<body>  
 
 
     <div class="container ">
@@ -37,7 +37,7 @@ $select_query_result = mysqli_query($con, $select_query) or die(mysqli_errno($co
 <div class="col-xs-2">ID</div>
 <div class="col-xs-10"><?php echo $row['id'] . "<br>";?></div>
 
-</div>
+</div>  
 
 
 <div class="row">
@@ -50,11 +50,27 @@ $select_query_result = mysqli_query($con, $select_query) or die(mysqli_errno($co
 <div class="col-xs-2">Email-ID</div>
 <div class="col-xs-10"> <?php echo  $row['email_id']  . "<br>";?> </div>
 </div>
-      
-
+<div class="row">
+              <div class="col-xs-2">Products</div>
+              <div class="col-xs-10"><?php echo number_of_products_purchased($con, $row['id']); ?></div>
+          </div>
+          <hr>
         <?php } ?>
-<hr>
+
 
     </div>
 </body>
-</html>
+            
+</html> 
+
+
+<?php
+//data calculation using function
+function number_of_products_purchased($con, $user_id) {
+  $users_products_query = "SELECT  id FROM user_products WHERE user_id = '$user_id'";
+  $users_products_result = mysqli_query($con, $users_products_query);
+  $number_of_products = mysqli_num_rows($users_products_result);
+  return $number_of_products;
+  
+}
+?>
