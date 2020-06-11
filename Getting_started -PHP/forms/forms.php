f9a0e6dbd44ff591f6f77080af0503fdf87e9fbf
Swaraj961 <swarajroutray961@gmail.com>
2020-06-11 17:58:31 +0530

    new form

--- Getting_started -PHP/forms/forms.php
+++ Getting_started -PHP/forms/forms.php
@@ -1,60 +1,16 @@
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php html bootstrap  </title>
-    <!-- <link rel="stylesheet" href="style.css"> -->
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <script type="text/javascript" src="bootstrap/js/jquery-3.5.1.min.js "></script>
-
-    <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
-    <style>
-    .bg{
-        background-color: red;
-    }
-
-    .top-mar{
-        margin-top: 30px;
-    }
-    </style>
+    <title> <?php echo "Registration" ?> </title>
 </head>
-<body>  
-
-    </div>
-
-    <div class="container top-mar ">
-        <div class="row top_margin">
-            <div class="col-xs-6 col-xs-offset-3">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">User Registration</div>
-                    <div class="panel-body">
-                        <form method="POST" action="user_reg.php">
-                           
-                            <div class="form-group">
-                                <label for="name">Name</label>
-                                <input type="text" class="form-control" id="name" name="name" >
-                            </div>
-                            <div class="form-group">
-                                <label for="email">Email</label>
-                                <input type="email" class="form-control" id="email" name="email">
-                            </div>
-                           
-                            <div class="form-group">
-                                <label for="phone">Phone</label>
-                                <input type="text" class="form-control" id="phoneno" name="phoneno">
-                            </div>
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+<body>
+    <form action="user_reg.php" method="GET">
+        Email : <input type="text" placeholder="email" name="email">
+        First Name : <input type="text" placeholder="first name" name="fname">
+        Last Name : <input type="text" placeholder="last name" name="lname">
+        <button>submit</button>
+    </form>
 </body>
-            
-</html> 
-
-
+</html>
--- Getting_started -PHP/forms/user_reg.php
+++ Getting_started -PHP/forms/user_reg.php
@@ -1,16 +1,25 @@
-<?php 
+<?php
+        $con = mysqli_connect("localhost","root","","formtest")     
+        or die(mysqli_error($con));
 
-$con = mysqli_connect("localhost","root","","loginform") or die($con);
-// data insertion throught form working now
+        ///////////////     SAVING FORM DATA TO THE DATABASE. ///////////////////////
+
+        $email = mysqli_real_escape_string($con,$_GET['email']);       
+        $fname = mysqli_real_escape_string($con,$_GET['fname']);       
+        $lname = mysqli_real_escape_string($con,$_GET['lname']);       
     
-$name = $_POST['name'];
-$email = $_POST['email'];
-$phoneno = $_POST['phoneno'];
-$user_resgister_query = "INSERT  into users VALUES ('$name','$email','$phoneno')" ;
+        $userRegistrationQuery = "insert into users(email,first_name,last_name) values ('$email',
+                    '$fname','$lname')";      
+    
+        $userDetailsSubmit = mysqli_query($con,$userRegistrationQuery)      
+        or die(mysqli_error($con)); 
+        echo"user register succesfully";
 
+        ///////////////     DISPLAY DATA TO THE DATABASE. ///////////////////////
 
-$user_register_submit = mysqli_query($con,$user_resgister_query) ;
-echo"user register succesfully"
-
-
+        $select_query = "select * from users";        
+        $select_query_result = mysqli_query($con,$select_query)       
+        or die(mysqli_error($con));
+    
+        $total_rows_fetch = mysqli_num_rows($select_query_result);    
 ?>
